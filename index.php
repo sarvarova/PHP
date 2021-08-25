815ca36b261afd4fb66286f566dad385b81babb9
sarvarova <nsarwarova@gmail.com>
2021-08-25 23:25:57 +0300

    Добавлена домашняя работа 2

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include "./src/functions.php";
+
+task1(["банан","яблоко","киви"], false);
+
+echo task2('/', 1, 2, 0);
+
+echo task3(5, 7);
+
+echo task4();
+
+echo task5();
+
+echo task6('test.txt', 'Hello again!');
